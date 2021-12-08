3cf997c7a69feb25c9c9f8fdafd32a2028b72f95
SAKSHAM KUMAR JHA <72131625+SakshamKJ@users.noreply.github.com>
2021-12-08 08:41:54 +0530

    Create LinearAlgDemo.java

---
+++ LinearAlgebra/source/in/edu/scse/xim/linalg/LinearAlgDemo.java
@@ -0,0 +1,9 @@
+package in.edu.scse.xim.linalg;
+
+public class LinearAlgDemo implements RealMatrix{
+	public static void main(String args[]){
+		int rowDimension;
+		int columnDimension;
+		AbstractRealMatrix mtr = new AbstractRealMatrix(rowDimension, columnDimension);
+	}
+}
